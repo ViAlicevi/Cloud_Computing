e1b997625a2fd3bb01886a927b40474d9e2766a2
tvi26 <thivy2606@gmail.com>
2024-01-09 21:53:27 +0700

    updated

--- config/database.js
+++ config/database.js
@@ -1,6 +1,15 @@
 const { Sequelize } = require('sequelize');
-const { error } = require('console');
-module.exports = new Sequelize('codegig', 'postgres', '123456', {
-    host: 'localhost',
+
+const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASSWORD, {
+    host: process.env.DB_HOST,
     dialect: 'postgres'
-  });
+});
+
+module.exports = sequelize;
+
+// const { Sequelize } = require('sequelize');
+// const { error } = require('console');
+// module.exports = new Sequelize('codegig', 'postgres', '123456', {
+//     host: 'localhost',
+//     dialect: 'postgres'
+//   });
